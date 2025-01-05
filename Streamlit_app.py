c74926d2d3da931ebc84a252b629c2c2744df0ad
Philippe Vivier <pvivier@gmail.com>
2025-01-05 20:52:27 +0100

    Update Streamlit_app.py

--- Streamlit_app.py
+++ Streamlit_app.py
@@ -1,6 +1,10 @@
 # Import python packages
 import streamlit as st
 from snowflake.snowpark.functions import col
+import requests
+
+smoothiefruit_response = requests.get("https://www.smoothiefroot.com/api/fruit/orange")
+st.write(smoothiefruit_response)
 
 # Write directly to the app
 st.title("Customize your smoothie Streamlit App")
